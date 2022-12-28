a097b59c607c2d25c0f704fadabd9710de1d0009
Keito654 <keito19981129@gmail.com>
2022-12-29 02:54:05 +0900

    全角半角変換機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { FullHalfConvert } from 'components/FullHalfConvert';
 import { FC } from 'react';
+import { Box, Container } from '@mui/material';
+import { FullHalfConvert } from 'components/FullHalfConvert';
 
 export const App: FC = () => {
   return (
-    <>
-      <FullHalfConvert />
-    </>
+    <Container fixed>
+      <Box sx={{ my: 4 }}>
+        <FullHalfConvert />
+      </Box>
+    </Container>
   );
 };
--- src/components/FullHalfConvert.tsx
+++ src/components/FullHalfConvert.tsx
@@ -1,6 +1,67 @@
-import { TextField } from '@mui/material';
-import { FC } from 'react';
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { FC, useState } from 'react';
+import { Box, Button, TextField } from '@mui/material';
+
+const convertFullToHalf = (word: string): string => {
+  return word.replace(/[Ａ-Ｚａ-ｚ０-９]/g, (x) => {
+    return String.fromCharCode(x.charCodeAt(0) - 0xfee0);
+  });
+};
+
+const convertHalfToFull = (word: string): string => {
+  return word.replace(/[A-Za-z0-9]/g, (x) => {
+    return String.fromCharCode(x.charCodeAt(0) + 0xfee0);
+  });
+};
 
 export const FullHalfConvert: FC = () => {
-  return <TextField label="テキスト" multiline />;
+  const [inputValue, setInputValue] = useState<string | undefined>(undefined);
+  const [convertedValue, setConvertedValue] = useState<string>('');
+
+  const handleClickFullToHalf = () => {
+    setConvertedValue(convertFullToHalf(inputValue ?? ''));
+  };
+
+  const handleClickHalfToFull = () => {
+    setConvertedValue(convertHalfToFull(inputValue ?? ''));
+  };
+
+  return (
+    <>
+      <Box sx={{ fontSize: '2rem', marginBottom: '1ch' }}>全角半角変換</Box>
+      <Box>変換前のテキスト</Box>
+      <TextField
+        label="テキスト"
+        multiline
+        rows={4}
+        fullWidth
+        margin="normal"
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          margin: '1ch 0',
+        }}
+      >
+        <Button variant="contained" onClick={handleClickHalfToFull}>
+          全て全角文字に変換
+        </Button>
+        <Button variant="contained" onClick={handleClickFullToHalf}>
+          全て半角文字に変換
+        </Button>
+      </Box>
+      <TextField
+        label="変換後"
+        multiline
+        rows={4}
+        value={convertedValue}
+        fullWidth
+        margin="normal"
+      ></TextField>
+    </>
+  );
 };
