670fd26225257d85db64111896bf3ff81c319dfa
Keito654 <keito19981129@gmail.com>
2022-12-29 01:01:19 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { FullHalfConvert } from 'components/FullHalfConvert';
+import { FC } from 'react';
+
+export const App: FC = () => {
+  return (
+    <>
+      <FullHalfConvert />
+    </>
+  );
+};
---
+++ src/components/FullHalfConvert.tsx
@@ -0,0 +1,6 @@
+import { TextField } from '@mui/material';
+import { FC } from 'react';
+
+export const FullHalfConvert: FC = () => {
+  return <TextField label="テキスト" multiline />;
+};
